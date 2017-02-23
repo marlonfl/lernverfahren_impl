d5332ff7e5466e4abcd14fe615ded13271502750
marlonfl <marlonfl@informatik.uni-bremen.de>
2017-02-23 22:41:07 +0100

    fix bug

--- clustering/kmeans.py
+++ clustering/kmeans.py
@@ -3,47 +3,39 @@ import math
 import random
 import matplotlib.pyplot as plt
 
-def init_centers(X_train, k):
-    dim_limits = []
-    centers = []
-    for i in range(len(X_train[0])):
-        fvals = X_train[:,i]
-        dim_limits.append((min(fvals), max(fvals)))
 
-    for k_i in range(k):
-        center = []
-        for mini, maxi in dim_limits:
-            center.append(random.uniform(mini,maxi))
-        centers.append(center)
+class Center(object):
+    coordinates = np.array([])
 
-    return centers
+    def __init__(self, dimensions):
+        self.coordinates = np.array([random.uniform(0, 1)
+                                        for i in range(dimensions)])
+    def update_position(self, assigned):
+        self.coordinates = np.mean(assigned, axis=0)
+
+
+def init_centers(k, dim):
+    return [Center(dim) for i in range(k)]
 
 def train(X_train, k, iterations):
-    centers = init_centers(X_train, k)
-    print ("initial centers " + str(centers))
+    centers = init_centers(k, len(X_train[0]))
+
     for i in range(iterations):
-        assignments = [[]] * k
+        assignments = [[] for i in range(k)]
         for sample in X_train:
-            assignments[assign_to_center(centers, sample)].append(sample)
+            a_c = assign_to_center(centers, sample)
+            assignments[a_c].append(sample)
 
-        centers = update_centers(assignments)
+        for i, assigned in enumerate(assignments):
+            centers[i].update_position(assigned)
 
-    return centers
-
-def update_centers(assignments):
-    centers = []
-    for i in range(len(assignments)):
-        center = []
-        for dim in range(len(assignments[0][0])):
-            center.append(np.mean(np.array(assignments)[i][:,dim]))
-        centers.append(center)
-    print ("Centers updated to: " + str(centers))
     return centers
 
 def assign_to_center(centers, sample):
     dists = []
     for center in centers:
-        dists.append(euclid(center, sample))
+        dists.append(euclid(center.coordinates, sample))
+
     return dists.index(min(dists))
 
 def predict():
@@ -79,14 +71,16 @@ if __name__ == "__main__":
     # X_test = load_unsupervised(sys.argv[4])
 
 
-    train_cl1 = np.array([np.array([random.uniform(0, 0.45), random.uniform(0, 0.6)]) for i in range(100)])
-    train_cl2  = np.array([np.array([random.uniform(0.55, 1), random.uniform(0.4, 1)]) for i in range(100)])
+    train_cl1 = np.array([np.array([random.uniform(0, 0.45), random.uniform(0, 0.6)]) for i in range(20)])
+    train_cl2  = np.array([np.array([random.uniform(0.55, 1), random.uniform(0.4, 1)]) for i in range(20)])
     train_data = np.concatenate((train_cl1, train_cl2), axis=0)
-    centers = train(train_data, 3, 10)
-    # print (centers)
-    #
-    #plt.scatter(*zip(*train_cl1),color='red')
-    #plt.scatter(*zip(*train_cl2),color='blue')
-    plt.scatter(*zip(*centers), color='green', s=200)
+    centers = train(train_data, 2, 10)
+    for c in centers:
+        print (c.coordinates)
+
+    c_coords = [c.coordinates for c in centers]
+    plt.scatter(*zip(*train_cl1),color='red')
+    plt.scatter(*zip(*train_cl2),color='blue')
+    plt.scatter(*zip(*c_coords), color='green', s=200)
     plt.ylabel('some numbers')
     plt.show()
