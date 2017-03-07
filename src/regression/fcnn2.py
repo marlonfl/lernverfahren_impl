eb0f4457af55b985b5e94b07e387ace94cab8e11
Marlon Flügge <marlonfl@fb3.uni-bremen.de>
2017-03-07 16:11:45 +0100

    make fcnn more general, cleap up etc

--- src/regression/fcnn2.py
+++ src/regression/fcnn2.py
@@ -1,35 +1,67 @@
 import numpy as np
+import sys
 
-class Fully_Connected_NN(object):
-    def __init__(self):
-        self.input_neurons = 2
-        self.output_neurons = 1
-        self.hidden_layers = 1
-        self.hidden_neurons = 3
+class FCNN(object):
+    def __init__(self, i_n, o_n, h_n, h_l, func):
+        self.ACT_FUNCS = {'sigmoid': self.sigmoid}
+        self.ACT_FUNCS_D = {'sigmoid': self.sigmoid_der}
 
-        self.W1 = np.random.rand(self.input_neurons,
+        if func not in self.ACT_FUNCS.keys():
+            sys.exit("Unknown Activation Function: %s" % (func))
+
+        if 0 in [i_n, o_n, h_n, h_l]:
+            sys.exit("Net Topology Parameters can't be 0")
+
+        print ("---Topology---")
+        print (("Type: %s\nInput Dimension: %s\nOutput Dimension %s\nHidden "
+               + "Layers: %s\nHidden Layer Neurons: %s\nActivation Function: %s")
+               % ("Fully Connected", i_n, o_n, h_l, h_n, func))
+        print ("--------------")
+
+        # topology
+        self.input_neurons = i_n
+        self.output_neurons = o_n
+        self.hidden_neurons = h_n
+        self.hidden_layers = h_l
+        self.activate = self.ACT_FUNCS[func]
+        self.activate_der = self.ACT_FUNCS_D[func]
+
+        # initialize weights randomly
+        self.W_in = np.random.rand(self.input_neurons,
                                  self.hidden_neurons)
 
-        self.W2 = np.random.rand(self.hidden_neurons,
+        self.W_out = np.random.rand(self.hidden_neurons,
                                  self.output_neurons)
+
+        self.hidden_weights = []
+        for layer in range(self.hidden_layers - 1):
+            self.hidden_weights.append(np.random.rand(
+                self.hidden_neurons, self.hidden_neurons))
+
 
     # 1 matrix row = 1 training example
     def forward(self, X):
-        # sum for every neuron for every input in 1st hidden layer
-        self.z2 = np.dot(X, self.W1)
-        # activation on every neuron for every input
-        self.a2 = self.sigmoid(self.z2)
+        # propragating from input through 1st hidden layer
+        prev_layer_output = self.activate(np.dot(X, self.W_in))
 
-        self.z3 = np.dot(X, self.a2)
-        Y_pred = self.sigmoid(self.z3)
+        # propragating through hidden layers
+        for l_weights in self.hidden_weights:
+            prev_layer_output = self.activate(np.dot(prev_layer_output, l_weights))
 
-        return Y_pred
+        # propragating from last hidden through output layer
+        pred = self.activate(np.dot(prev_layer_output, self.W_out))
+
+        return pred
 
     def sigmoid(self, z):
-        return 1/1(np.exp(-z))
+        return 1/(1 + np.exp(-z))
+
+    def sigmoid_der(self, z):
+        pass
 
     def calc_error(self, predictions, y):
+        pass
 
 if __name__ == "__main__":
-    nn = Fully_Connected_NN()
-    print (nn.forward())
+    nn = FCNN(2, 1, 3, 1, 'sigmoid')
+    print (nn.forward(np.random.rand(2,2)))
